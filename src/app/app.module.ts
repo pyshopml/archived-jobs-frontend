827fd462c0d1e41db384a353d2f92195004b174e
Danil Migachev <dowhileman@gmail.com>
2017-01-11 16:37:11 +0300

    jobs-015: add project's files

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { AppRoutingModule } from './app-routing.module'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
