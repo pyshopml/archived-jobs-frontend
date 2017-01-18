010f7a1297403b8b09c72956e3b9332a8c6b584b
Danil Migachev <dowhileman@gmail.com>
2017-01-18 18:45:37 +0300

    jobs-018: install angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
 
 // backend substitution
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -14,6 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { VacanciesComponent } from './vacancies/vacancies.component';
 import { VacancyCreationComponent } from './vacancy-creation/vacancy-creation.component';
 import { VacancyDetailComponent } from './vacancy-detail/vacancy-detail.component';
+
 
 import { VacanciesService } from './services/vacancies.service';
 import { HeaderComponent } from './header/header.component';
@@ -35,6 +37,7 @@ import { HomeComponent } from './home/home.component';
     HttpModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    MaterialModule.forRoot(),
     
     InMemoryWebApiModule.forRoot(InMemoryDataService), // backend substitution
   ],
